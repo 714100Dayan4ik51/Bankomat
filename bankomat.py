c36a0e68eb01004a7a9bab7832bef7fbd98458ad
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-10-13 19:04:58 +0500

    Сделал вывод данных, обеспечил надежность ввода данных

--- bankomat.py
+++ bankomat.py
@@ -1,13 +1,25 @@
 from num2words import num2words
 
+
 def number_to_words(num):
-    if num < 1 and num > 100000:
-        return "Сумма должна быть равна от 1 до 100 000"
-    words = num2words(num, lang = 'ru')
-    ending = num%10
-    if ending == 1 and num% 100 != 11:
-        end = "рубль"
-    elif 1 < ending < 5 and (num%100 < 10 or num %100 >21):
-        end = "рубля"
+    words = num2words(num, lang='ru')
+    ending = num % 10
+    if ending == 1 and num % 100 != 11:
+        end = " рубль"
+    elif 1 < ending < 5 and (num % 100 < 10 or num % 100 > 21):
+        end = " рубля"
     else:
-        end = "рублей"
+        end = " рублей"
+    result = words.capitalize() + end
+    return result
+
+
+def main():
+    while True:
+        try:
+            num = int(input("Введите сумму денег от 1 до 100 000 - "))
+            print(number_to_words(num))
+            break
+        except ValueError:
+            print("Вы должны ввести число от 1 до 100 000, поробуйте снова. ")
+main()
