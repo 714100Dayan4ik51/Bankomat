6557e04b738ccbb6d9136aa52dbfe94b14807d41
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-10-13 18:51:33 +0500

    делаем проверку, для правильного окончания валюты

--- bankomat.py
+++ bankomat.py
@@ -5,4 +5,9 @@ def number_to_words(num):
         return "Сумма должна быть равна от 1 до 100 000"
     words = num2words(num, lang = 'ru')
     ending = num%10
-    
+    if ending == 1 and num% 100 != 11:
+        end = "рубль"
+    elif 1 < ending < 5 and (num%100 < 10 or num %100 >21):
+        end = "рубля"
+    else:
+        end = "рублей"
